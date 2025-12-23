9a83070e49f094858c3d3513d4243f58ed9b59c2
gsbharani <gsbharani45@gmail.com>
2025-12-23 14:07:33 +0530

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.routes.channel import router as channel_router
-
-app = FastAPI(title="YouTube Analytics API")
-
-app.include_router(channel_router, prefix="/channel")
