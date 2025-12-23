b870c4be900af93119344ab626b2eb8ad4d5b6fe
gsbharani <gsbharani45@gmail.com>
2025-12-23 14:07:52 +0530

    Delete crud.py

--- crud.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from .database import get_cursor
-
-def insert_metrics(stats):
-    cursor = get_cursor()
-    cursor.execute("""
-        EXEC dbo.sp_InsertDailyChannelMetrics ?, ?, ?, ?, ?
-    """, stats["channel_id"], stats["subs"], stats["views"], 0, 0)
-    cursor.commit()
