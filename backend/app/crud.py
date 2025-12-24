dc126d711e0238ca01df26279c956029e4fef163
gsbharani <gsbharani45@gmail.com>
2025-12-24 10:08:03 +0530

    Delete backend/app/crud.py

--- backend/app/crud.py
+++
@@ -1,9 +0,0 @@
-from app.database import get_cursor
-
-conn, cursor = get_cursor()
-
-cursor.execute("EXEC dbo.sp_InsertDailyChannelMetrics %s, %s, %s, %s, %s",
-               (channel_id, subscribers, views, watch_time, revenue))
-conn.commit()
-cursor.close()
-conn.close()
